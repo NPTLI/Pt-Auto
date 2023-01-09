481bf476f18c4fae2eea0100b80840361b456c37
ListenHua <listenhua@ListenHuadeMacBook-Air.local>
2023-01-09 13:32:51 +0800

    fix:框架搭建

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,7 +1,18 @@
-import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-import router from "./router/index";
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import piniaStore from './store';
+import ArcoVue from '@arco-design/web-vue';
+import '@arco-design/web-vue/dist/arco.css';
+import '/@/styles/index.less';
+import '/@/styles/reset.less';
+import 'uno.css';
+
+// 支持SVG
+import 'virtual:svg-icons-register';
 
 const app = createApp(App);
-app.use(router).mount("#app");
+app.use(ArcoVue)
+app.use(router);
+app.use(piniaStore);
+app.mount('#app');
--- web/src/router/index.ts
+++ web/src/router/index.ts
@@ -1,25 +1,25 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
-// 1. 定义路由组件.
-// 也可以从其他文件导入
-import Home from "@/views/index.vue";
+import { createRouter, createWebHashHistory } from 'vue-router';
+import routes from 'virtual:generated-pages';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
-// 2. 定义一些路由
-// 每个路由都需要映射到一个组件。
-// 我们后面再讨论嵌套路由。
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: "/",
-    component: Home,
-  },
-];
-
-// 3. 创建路由实例并传递 `routes` 配置
-// 你可以在这里输入更多的配置，但我们在这里
-// 暂时保持简单
+routes.push({
+  path: '/',
+  redirect: '/login',
+});
+//导入生成的路由数据
 const router = createRouter({
-  // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
   history: createWebHashHistory(),
-  routes, // `routes: routes` 的缩写
+  routes,
+});
+
+router.beforeEach(async (_to, _from, next) => {
+  NProgress.start();
+  next();
+});
+
+router.afterEach((_to) => {
+  NProgress.done();
 });
 
 export default router;
